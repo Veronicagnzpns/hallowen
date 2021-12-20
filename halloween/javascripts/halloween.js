25b357c1ad8f547b9b675d0e04eef6f93667ba4e
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2021-12-20 13:20:17 +0100

    Add files via upload

---
+++ halloween/javascripts/halloween.js
@@ -0,0 +1,7 @@
+/**********   Aquí están las funciones de halloween  ******/
+
+/********** función mostrar *****************/
+
+function mostrar(algo){
+    document.getElementById(algo).style.visibility="visible";
+}
---
+++ javascript/halloween.js
@@ -0,0 +1,13 @@
+/**********   Aquí están las funciones de halloween  ******/
+
+/********** función mostrar *****************/
+
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible";
+}          
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden";
+}          
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
